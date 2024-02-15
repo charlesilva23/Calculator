08c9703801980851d4f7907600cc7871ebe892af
unknown <charlesilva2323@gmail.com>
2024-02-14 21:23:39 -0300

    Calculadora simples

--- src/components/calculator/calculator.jsx
+++ src/components/calculator/calculator.jsx
@@ -35,7 +35,7 @@ function Calculator() {
         setOldValue(value);
         setValue(0);
     }
-    
+
     function changeSign() {
 
         if (value > 0) {
@@ -89,3 +89,74 @@ function Calculator() {
 }
 
 export default Calculator;
+
+
+/*
+const initialState = {
+    displayValue: "0",
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
+
+function Calculadora(){
+    
+    const [state, setState] = useState({ ...initialState })
+
+    const [memory, setMemory] = useState(null);
+    const [operation, setOperation] = useState(null);
+    const [currentValue, setCurrentValue] = useState('');
+
+    const clearMemory = () => {
+        setState({ ...initialState })
+    }
+
+    const setOperation = (operation) => {
+        if(state.current === 0){
+            setState({ operation, current: 1,clearDisplay: true})
+        } else {
+            const equals = operation === "="
+            const currentOperation = state.operation
+
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e) {
+                values[0] = this.state.values[0]
+            }
+
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
+    }
+
+    const addDigit = (n) => {
+        if(n === '.' && state.displayValue.includes('.')) {
+            return
+        }
+
+        const clearDisplay = state.displayValue === "0" || state.clearDisplay;
+        const currentValue = clearDisplay ? '' : state.displayValue;
+        const displayValue = currentValue + n 
+        setState({ displayValue, clearDisplay: false })
+
+
+        if(n !== '.'){
+            const i = state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...state.values] 
+            values[i] = newValue
+            setState({ values })
+            console.log(values)
+        }
+    }
+ */
